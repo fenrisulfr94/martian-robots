0dba0eca354473be93cf3b70678940903b012e96
Gareth Gill <garethmgill@gmail.com>
2014-03-13 15:13:17 +0000

    Tidied up code a little.

--- robot.hs
+++ robot.hs
@@ -9,16 +9,16 @@ type Position = (Integer, Integer)
 
 -- Direction a robot is facing
 data Bearing = N -- North
-			 | S  -- South
-			 | E  -- East
-			 | W  -- West
-			 deriving (Eq, Show)
+		| S  -- South
+     	| E  -- East
+		| W  -- West
+		deriving (Eq, Show)
 
 -- Different kinds of instructions a robot can perform
 data Instruction = L  -- Left
-				 | R  -- Right
-				 | F  -- Forward
-				 deriving (Eq, Show)
+			| R  -- Right
+			| F  -- Forward
+			deriving (Eq, Show)
 
 -- A robot is lost if it moves off the grid size 
 -- defined by the user.
@@ -36,30 +36,30 @@ type Scent = [(Position, Bearing)]
 -- Handle 'L' instructions
 rotateLeft :: Robot -> Robot
 rotateLeft ((x, y), b, l)
-				| b == N = ((x, y), W, l)  
-				| b == S = ((x, y), E, l)
-				| b == E = ((x, y), N, l)
-				| b == W = ((x, y), S, l)
+		| b == N = ((x, y), W, l)  
+		| b == S = ((x, y), E, l)
+		| b == E = ((x, y), N, l)
+		| b == W = ((x, y), S, l)
 
 -- Handle 'R' instructions
 rotateRight :: Robot -> Robot
 rotateRight ((x, y), b, l) 
-				| b == N = ((x, y), E, l)
-				| b == S = ((x, y), W, l)
-				| b == E = ((x, y), S, l)
-				| b == W = ((x, y), N, l)
+		| b == N = ((x, y), E, l)
+		| b == S = ((x, y), W, l)
+		| b == E = ((x, y), S, l)
+		| b == W = ((x, y), N, l)
 
 -- Move robot forward with respect to its bearing
 forward :: Robot -> Robot
 forward ((x, y), b, l) 
-				| b == N = ((x + 1, y), b, l)  
-				| b == S = ((x - 1, y), b, l)
-				| b == E = ((x, y + 1), b, l)
-				| b == W = ((x, y + 1), b, l)
+		| b == N = ((x + 1, y), b, l)  
+		| b == S = ((x - 1, y), b, l)
+		| b == E = ((x, y + 1), b, l)
+		| b == W = ((x, y + 1), b, l)
 
 move :: Robot -> Instruction -> Robot
 move r instr 
-		| instr == L = rotateLeft r
-		| instr == R = rotateRight r
-		| instr == F = forward r
+	| instr == L = rotateLeft r
+	| instr == R = rotateRight r
+	| instr == F = forward r
 
