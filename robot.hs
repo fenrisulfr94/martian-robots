9c47717e5b460d6ec5b7b06097cb7edb99a3f63e
Gareth Gill <garethmgill@gmail.com>
2014-03-18 13:19:45 +0000

    Converted guard statements to case statement.  Much more readable now.

--- robot.hs
+++ robot.hs
@@ -10,7 +10,7 @@ type Position = (Integer, Integer)
 -- Direction a robot is facing
 data Bearing = N -- North
 		| S  -- South
-     	| E  -- East
+		| E  -- East
 		| W  -- West
 		deriving (Eq, Show)
 
@@ -37,35 +37,40 @@ type Scent = [(Position, Bearing)]
 -- Rotate robot counter-clockwise with respect to 
 -- its bearing
 left :: Robot -> Robot
-left ((x, y), b, l)
-		| b == N = ((x, y), W, l)  
-		| b == S = ((x, y), E, l)
-		| b == E = ((x, y), N, l)
-		| b == W = ((x, y), S, l)
+left ((x, y), b, l) =
+	case b of
+		N -> ((x, y), W, l)
+		S -> ((x, y), E, l)
+		E -> ((x, y), N, l)
+		W -> ((x, y), S, l)
 
 -- Handle 'R' instructions
 -- Rotate robot clockwise with respect to its bearing
 right :: Robot -> Robot
-right ((x, y), b, l) 
-		| b == N = ((x, y), E, l)
-		| b == S = ((x, y), W, l)
-		| b == E = ((x, y), S, l)
-		| b == W = ((x, y), N, l)
+right ((x, y), b, l) =
+	case b of
+		N -> ((x, y), E, l)
+		S -> ((x, y), W, l)
+		E -> ((x, y), S, l)
+		W -> ((x, y), N, l)
 
 -- Move robot forward with respect to its bearing
 forward :: Robot -> Robot
-forward ((x, y), b, l) 
-		| b == N = ((x, y + 1), b, l)  
-		| b == S = ((x, y - 1), b, l)
-		| b == E = ((x + 1, y), b, l)
-		| b == W = ((x - 1, y), b, l)
+forward ((x, y), b, l) = 
+	case b of
+		N -> ((x, y + 1), b, l)  
+		S -> ((x, y - 1), b, l)
+		E -> ((x + 1, y), b, l)
+		W -> ((x - 1, y), b, l)
 
 move :: Robot -> Instruction -> Robot
-move r instr 
-	| instr == L = left 	r
-	| instr == R = right 	r
-	| instr == F = forward 	r
+move r instr =
+	case instr of
+		L -> left 		r
+		R -> right 		r
+		F -> forward 	r
 
+-- Fold 'move' over a single robot and its input
 exec :: Robot -> [Instruction] -> Robot
 exec r is = foldl move r is 
 
